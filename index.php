d5311d7abf639fec758e89af58cb750770b3ce70
Pikesh Patel <Pikesh.11606271@lpu.in>
2018-12-18 19:13:47 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,229 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="canonical" href="index.html" itemprop="url" /> <title> Happy New Year 2019</title>
+<meta name="description" content="Create Happy Raksha Bandhan SMS, raksha bandhan messages, raksha bandhan images, raksha bandhan 2018, raksha bandhan, rakhi message, rakhi wishes, raksha bandhan status, raksha bandhan in hindi, rakhi date, raksha bandhan quotes 2018 wishing website" />
+<link rel="icon" href="image/rakhi-logo.png">
+<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5b486eaae1ceeb001b842d49&product=sticky-share-buttons' async='async'></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css ">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+
+</head>
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+
+
+<body>
+
+
+ <center>
+
+<!-- header_320_50 -->
+
+
+</center>
+
+<!-- Sidebar Sliding Images --->
+<marquee class="slide1" behavior="scroll" direction="down" scrolldelay="5">  <br>
+<img src="image/rakhi-blue.png" width="40px"/><br><br>
+<img src="image/rakhi-red.png" width="40px"/><br><br>
+<img src="image/rakhi-blue.png" width="40px"/><br><br>
+<img src="image/rakhi-red.png" width="40px"/><br><br>
+<img src="image/rakhi-blue.png" width="40px"/><br><br>
+</marquee>
+<marquee class="slide" behavior="scroll" direction="up" scrolldelay="5">  <br>
+<img src="image/rakhi-red.png" width="40px"/><br><br>
+<img src="image/rakhi-blue.png" width="40px"/><br><br>
+<img src="image/rakhi-red.png" width="40px"/><br><br>
+<img src="image/rakhi-blue.png" width="40px"/><br><br>
+<img src="image/rakhi-red.png" width="40px"/><br><br>
+</marquee>
+<center><p id="demo"></p></center>
+<h1 style="display:none;"> Happy Raksha Bandhan Script </h1>
+<div class="container">
+<div class="row">
+<div class="col-xs-12">
+<div class="msg-holder">
+<div class="msg">
+<?php
+
+//$n=$_GET['by'];
+$n=str_replace('-', ' ', str_replace('+', ' ', $_GET['by']));
+
+echo "$n"
+
+?> </div>
+</div>
+<div class="msg2"> Wishing You </div>
+<div class="msg3"> Happy New Year 2019 </div>
+
+<center style="margin-top:10px;"><audio controls autoplay>
+ <source src="rakhi-song.mp3" type="audio/mpeg">
+</audio></center>
+</div>
+</div>
+<div class="row">
+<div class="col-xs-12">
+<h6 class="myimg"> <img src="image/2019.gif" class='img-responsive' alt="Happy Independence 2018"> </h6>
+</div>
+</div>
+</div>
+
+    <center><p style="color:#ffffff; margin:20px 0">ज़िन्दगी हो जाये सुहानी, नए साल में<br>
+बात हो दिल की ज़ुबानी, नए साल में,<br><br>
+
+हर दिन हसीं और रातें रोशन हो,<br>
+खुशियों की हो रवानी, नए साल में,
+<br><br>
+हर किसी के दिल में हो, सबके लिए प्यार<br>
+पूरी हो अधूरी कहानी, नए साल में<br><br>
+
+करते हैं हम ये दुआ, सिर को झुकाकर<br>
+मिले गरीब को रोटी और पानी, नए साल में<br><br>
+
+पुराना साल हो रहा हैं, अब सबसे दूर<br>
+ख़त्म हो नफरतो की कहानी, नए साल में<br><br>
+
+आपको तहे दिल से नववर्ष की शुभकामना
+<br><br>
+<b>आपको तहे दिल से नववर्ष की शुभकामना।</b></p> </center>
+  <p class="myimg"> <img src="image/raksha-bandhan-image.png" class='img-responsive' alt="Happy Raksha Bandhan"> </p>
+  
+  <!--<div align="center"><div align="center"><script language="JavaScript" src="http://clickmasters.mobie.in/Jss/effect/agve-loverain.red.js"></script></div></div>  <div align="center"><div id="mainDiv">	<marquee scrollamount="3" loop="infinite" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0"> <span style="background:	#FF0000;padding: 1px 7px;font-size: large;font-weight: bold;color: #FFF8DC;margin: 0px 0px;border-radius: 40px;"><font color="#F0FFFF">Feel the pride of being the part of such a glorious nation. Here’s sending my warm patriotic wishes to make this day truly memorable. </font></span><span style="background:#0000FF;padding: 1px 7px;font-size: large;font-weight: bold;color: #FF0000;margin: 0px 0px;border-radius: 40px;"><font color="white">Independence is a Precious gift of God. May We Always Remain Independent A Very Happy Raksha Bandhan To You.</font></span><span style="background:	Pink;padding: 1px 7px;font-size: large;font-weight: bold;color: #00FF00;margin: 0px 0px;border-radius: 40px;"><font color="#000000">Happy Indian Independence Day 2018</font></span></marquee></div></div>-->
+  
+  
+    
+    
+    
+<div class="container myads">
+<div class="row">
+<div class="col-xs-12">
+</div>
+</div>
+</div>
+<div class="container-fluid" id="box">
+<div class="row box2">
+<div class="col-xs-12 col-md-6">
+<form method="get" class="form-inline myform">
+<div class="form-group">
+<input type="text" class="form-control mytext animated pulse infinite input-lg" name="by" placeholder="Enter Your Name" required>
+</div>
+
+
+   <center>
+	
+	
+<!-- responsive -->
+
+
+</center>
+
+
+<button type="submit" class="btn btn-default btn-lg mybtn">Create With Your Name</button>
+</form>
+</div>
+  
+  
+<div class="col-xs-12 col-md-6">
+<div class="tt">
+<span class="sb-title"> <i class="fa fa-share-alt" aria-hidden="true"></i> Share With Your Friends </span>
+<span class="sb-share">
+<div class="wp sb-icon btn btn-lg whatsapp animated pulse infinite"> <a href="whatsapp://send?text= *Happy Raksha Bandhan In Advance* From <?php
+
+$n=$_GET['by'];
+
+echo "*$n*"
+
+?>
+&#10;
+
+%20Created%20Something%20Special%20Only%20For%20You%20Touch%20The%20Blue%20Line <?php 
+$actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+echo str_replace('+', '-', $actual_link); 
+?>" target="_blank" title="Share this post on Whatsapp" class="whatsapp btn btn-lg"> <i class="fa fa-whatsapp" aria-hidden="true"></i> Whatsapp </a> </div>
+
+
+
+  
+</span>
+</div>
+</div>
+<div class="col-xs-12">
+
+ 
+<center>
+	
+	
+<!-- responsive_text -->
+
+
+</center>
+ 
+
+    
+
+</div>
+</div>
+</div>
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!--<script src="/js/bird.js"></script>-->
+
+    
+
+    
+
+<script src="myjs.js"></script>
+<script type="text/javascript" src="doodle.js"></script>
+<!--<script type="text/javascript">
+		doodle.init("jet.png");
+	</script>-->
+	
+
+ 
+ 
+<!----Countdown time Setting ---->
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("august 26, 2018 00:00:00").getTime();
+
+// Update the count down every 01 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "<i> Days,</i> " + hours + " <i>Hrs,</i> "
+  + minutes + "<i> Min,</i> " + seconds + "<i> Sec</i> ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>  
+</body>
+</html>
