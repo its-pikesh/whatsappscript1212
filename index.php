c75125d8905414d19094c41a6a2975bf8aa40725
Pikesh Patel <Pikesh.11606271@lpu.in>
2018-12-19 09:29:29 +0530

    Update index.php
    Header ad added

--- index.php
+++ index.php
@@ -28,6 +28,13 @@
  <center>
 
 <!-- header_320_50 -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: "ca-pub-9387592169234168",
+          enable_page_level_ads: true
+     });
+</script>
 
 
 </center>
